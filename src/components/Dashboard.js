f7f516c065738787355dfe2ac75cce31875be183
Muraro Jeff <jeff@muraro.dev>
2020-08-11 18:13:27 +0200

    Display QRCode on Dashboard - Install package react-qr-code - Use the urlName for the redirection and the url

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,16 +3,30 @@ import React, {Component} from 'react';
 import {
     useParams
 } from "react-router-dom";
-import {Button, Card, Container, Form} from "react-bootstrap";
-import {FaChevronRight} from "react-icons/fa";
+import {Card, Container} from "react-bootstrap";
+import {readByUrlName} from "../api/Restaurant";
+
+import QRCode from "react-qr-code";
 
 class Dashboard extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            name: this.props.restaurantName,
+            name: '',
+            address: '',
+            urlName: this.props.restaurantUrlName
         }
+    }
+
+    componentDidMount() {
+        this._getData()
+    }
+
+    _getData = async () => {
+        const restaurant = await readByUrlName(this.state.urlName)
+        console.log(restaurant)
+        await this.setState({name: restaurant.name, address: restaurant.address})
     }
 
     render() {
@@ -21,7 +35,7 @@ class Dashboard extends Component {
                 <Card className={"login-card text-center"}>
                     <Card.Body>
                         <Card.Title className={"mb-4"}><h3>Hello {this.state.name}</h3></Card.Title>
-
+                        <QRCode value={"https://www.retrocov.ch/" + this.state.urlName} />
                     </Card.Body>
                 </Card>
             </Container>
@@ -32,6 +46,6 @@ class Dashboard extends Component {
 export default () => {
     const {restaurant} = useParams();
     return (
-        <Dashboard restaurantName={restaurant} />
+        <Dashboard restaurantUrlName={restaurant} />
     )
 }
